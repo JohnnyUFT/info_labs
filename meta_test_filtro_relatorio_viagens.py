3074b3483fa9d05ed42a89b41dc43c3c6d926a94
johnnyuft <johnnyuft@gmail.com>
2022-05-05 23:08:12 -0300

    Testes diversos com dataframe
    Django TestCase de Filtro por fazer

---
+++ meta_test_filtro_relatorio_viagens.py
@@ -0,0 +1,52 @@
+def _filter(self):
+	if self.ignition_on is not None:
+        self.df = self.df.query("hardware_ignition" == int(self.ignition))
+    if self.alert is not None:
+        if self.alert == "gps_speed":
+            self.df = self.df.query("speed" > self.speed)
+        else:
+            self.df = self.df.query(self.alert == 1)
+
+def build(self):
+    self._filter()
+    return self.df
+    
+
+# ------------------------------------ #
+
+from django.test import TestCase
+#from myapp.models import Reports
+from pandas import Dataframe
+
+class ReportTripCase(TestCase):
+    def setUp(self):
+        # definir os parametros
+        # pass
+        self.alert = None
+        self.speed = 84
+        self.ignition_on = True
+        
+        # a definir o Dataframe
+        
+    
+    def test_report_trip_filter_alert_none(self):
+        '''
+        Testa filtro: caso alert None
+        '''
+        self.assertEqual(self.alert, None)
+        
+    def test_report_trip_filter_alert_gps_speed(self):
+        '''
+        Testa filtro: caso alert == gps_speed
+        '''
+        self.assertEqual(self.alert, self.gps_speed)
+    
+    def test_report_trip_filter_alert_other(self):
+        '''
+        Testa filtro: caso alert not None and alert != gps_speed
+        Pode ser 1: 'anchor', 2: 'hardware_panic', 3: 'hardware_anti_theft_status'
+        '''
+        
+        # refatorar
+        self.df['columnn'] = 1 
+        self.assertEqual(self.df['column'], self.alert)
